486a97d45df86ee265d11761317ccc3e11174471
Arun Khatri <akcs4444@gmail.com>
2025-01-30 21:33:39 +0545

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div>customer</div>;
+};
+
+export default page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div>dashobard</div>;
+};
+
+export default page;
